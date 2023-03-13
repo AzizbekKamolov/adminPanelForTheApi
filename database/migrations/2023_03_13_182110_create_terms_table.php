385c5c1dece8a3d466590d3ca8f50ad538350eee
AzizbekKamolov <96434885+AzizbekKamolov@users.noreply.github.com>
2023-03-13 18:25:44 +0500

    fsdvrtgb

---
+++ database/migrations/2023_03_13_182110_create_terms_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTermsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('terms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categories', 'id');
+            $table->string('name_uz');
+            $table->string('name_ru')->nullable();
+            $table->string('name_en')->nullable();
+            $table->boolean('active_name_ru')->default(true);
+            $table->boolean('active_name_en')->default(true);
+            $table->integer('creted_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('terms');
+    }
+}
