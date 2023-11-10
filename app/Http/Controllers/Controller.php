f13c98251806d622ba36243216442654083e146f
AzizbekKamolov <96434885+AzizbekKamolov@users.noreply.github.com>
2023-11-10 17:56:52 +0500

    svd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function getDataWithPaginate($data)
+    {
+       return request()->per_page == 'all' ? $data->get() : (is_int(request()->per_page)) ? $data->paginate(request()->per_page) : $data->paginate();
+    }
 }
