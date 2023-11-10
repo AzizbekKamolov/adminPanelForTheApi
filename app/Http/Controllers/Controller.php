e4e2d2db25f6b0c799eb248d2eacecabb159fa7e
AzizbekKamolov <96434885+AzizbekKamolov@users.noreply.github.com>
2023-11-10 18:11:02 +0500

    svd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,6 +13,40 @@ class Controller extends BaseController
 
     public function getDataWithPaginate($data)
     {
-       return request()->per_page == 'all' ? $data->get() : (is_int(request()->per_page)) ? $data->paginate(request()->per_page) : $data->paginate();
+        return request()->per_page == 'all' ? $data->get() : (is_int(request()->per_page)) ? $data->paginate(request()->per_page) : $data->paginate();
+    }
+
+    public function success($message = 'success', $code = 200)
+    {
+        return response()->json([
+            "status" => 1,
+            "message" => $message,
+        ])->withHeaders(['code' => $code]);
+    }
+
+    public function successResponse($data = [], $message = 'success', $code = 200)
+    {
+        return response()->json([
+            "data" => $data,
+            "status" => 1,
+            "message" => $message,
+        ])->withHeaders(['code' => $code]);
+    }
+
+    public function error($message = 'error', $code = 422)
+    {
+        return response()->json([
+            "status" => 0,
+            "message" => $message,
+        ])->withHeaders(['code' => $code]);
+    }
+
+    public function errorResponse($data = [], $message = 'error', $code = 422)
+    {
+        return response()->json([
+            "errors" => $data,
+            "status" => 0,
+            "message" => $message,
+        ])->withHeaders(['code' => $code]);
     }
 }
